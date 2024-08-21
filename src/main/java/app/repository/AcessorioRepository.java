de8fac8485c29867ea3f127fd9d9e865ae01e5aa
samuelpd22 <samuel050101@hotmail.com>
2024-08-20 21:19:50 -0300

    feat: criando repository de acessorios

---
+++ src/main/java/app/repository/AcessorioRepository.java
@@ -0,0 +1,7 @@
+package app.repository;
+
+import app.entity.Acessorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcessorioRepository extends JpaRepository<Acessorio, Long> {
+}
